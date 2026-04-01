6509ce2a58e742d5a2352f1205a101ccace77870
wikitopian <wikitopian@pm.me>
2026-03-31 23:04:55 -0400

    refactor: #0 sacred commit

--- src/sql/functions/fidelityOf.js
+++ src/sql/functions/fidelityOf.js
@@ -12,15 +12,9 @@ export default function (scheme, state, turn) {
 	if (scheme === "http" || scheme === "https")
 		return turn > 0 ? "full" : "index";
 	if (
-		[
-			"system",
-			"reasoning",
-			"content",
-			"update",
-			"inject",
-			"keys",
-			"search",
-		].includes(scheme)
+		["system", "reasoning", "content", "update", "inject", "keys"].includes(
+			scheme,
+		)
 	)
 		return null;
 	return "full";
